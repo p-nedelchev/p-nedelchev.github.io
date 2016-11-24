854c400f2696deb1efb068994fbe48684ab63223
Petar Nedelchev <peter.krasimirov@gmail.com>
2016-11-24 10:20:18 +0200

    Calculator demo

---
+++ script.js
@@ -0,0 +1,46 @@
+/**
+ * Created by Peter Nedelchev <peter.krasimirov@gmail.com>.
+ */
+function calculateExpression(elementId) {
+    var expression = document.getElementById(elementId).value;
+    document.getElementById(elementId).value = eval(expression);
+}
+
+// function append(elementId, value) {
+//    document.getElementById(elementId).value += value;
+// }
+
+function clean(elementId) {
+    document.getElementById(elementId).value = "";
+}
+
+function clearLast(elementId) {
+    var value = document.getElementById(elementId).value;
+    document.getElementById(elementId).value = value.slice(0, value.length - 1);
+}
+
+
+function appendDigit(elementId, digit) {
+    document.getElementById(elementId).value += digit;
+}
+
+function appendOperand(elementId, operand) {
+    var currentExpression = document.getElementById(elementId).value;
+    var lastChar = currentExpression.slice(-1);
+    if(/(-|\+|\*|\/)/.test(lastChar) || currentExpression.length == 0) {
+        document.getElementById(elementId).value = currentExpression;
+        return;
+    }
+    document.getElementById(elementId).value += operand;
+}
+
+function appendDot(elementId) {
+    var currentExpression = document.getElementById(elementId).value;
+    var tokens = currentExpression.split(/[-+*/]/);
+    if(/(?=[.])/.test(tokens[tokens.length -1]) || currentExpression.length == 0) {
+        document.getElementById(elementId).value = currentExpression;
+        return;
+    }
+    document.getElementById(elementId).value += ".";
+}
+
